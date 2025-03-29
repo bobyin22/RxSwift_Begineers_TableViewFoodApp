8f15640324b815dbe0a74d279a7008d80ccf5ea1
bobyin <bobyin22@gmail.com>
2025-03-29 18:09:25 +0800

    init 建立一個UiKit 基本 TableView

--- RxSwift_Beginners_TableViewFoodApp/ViewController.swift
+++ RxSwift_Beginners_TableViewFoodApp/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    let tableViewItems = ["Item 1", "Item 2", "Item 3", "Item 4"]
+    
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTableView()
     }
 
+    func setupTableView() {
+        view.addSubview(tableView)
 
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableViewItems.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = tableViewItems[indexPath.row]
+        return cell
+    }
 }
 
